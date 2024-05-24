2985dfd61b2015d5f3e795fd1c9c312d82f40b51
Howw0 <kevin_lion@outlook.fr>
2024-05-24 17:51:23 +0200

    Setup lib

---
+++ setup.py
@@ -0,0 +1,32 @@
+from io import open
+from setuptools import setup, find_packages
+
+with open('requirements.txt', encoding="utf-8-sig") as f:
+    requirements = f.readlines()
+
+def readme():
+    with open('README.md', encoding="utf-8-sig") as f:
+        README = f.read()
+    return README
+
+classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+]
+
+setup(
+    name='ollama',
+    packages=find_packages(),
+    version='0.0.1',
+    install_requires=requirements,
+    license='MIT',
+    description='Connect to Ollama API',
+    long_description=readme,
+    author='Howw0',
+    url='https://github.com/Howw0/ollamaAPI',
+    download_url='https://github.com/Howw0/ollamaAPI.git',
+    keywords='ollama',
+    classifiers=classifiers,
+    packages=find_packages()
+    )
